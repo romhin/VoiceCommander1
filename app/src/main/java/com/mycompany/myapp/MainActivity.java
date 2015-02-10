41f766dc961e6e296e7870f143c556fccd6e0501
romhin <romhin25@gmail.com>
2015-02-10 22:27:54 +0000

    1

---
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -0,0 +1,18 @@
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+ {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.main);
+
+		
+
+	}
+
+
+}
